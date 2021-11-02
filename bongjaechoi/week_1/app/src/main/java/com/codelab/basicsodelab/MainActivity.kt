24b3050ad30fc000f5ec4f1f8f99452edd1a6b10
jay <bjchoi@lunasoft.co.kr>
2021-11-02 22:52:28 +0900

    상태 값 변경을 위한 버튼 클릭 구현

--- bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
+++ bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,13 +48,15 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
+        val expanded = remember { mutableStateOf(false) }
+
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = { }) {
-                Text(text = "Show More")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show More" else "Show Less")
 
             }
         }
