08ff8b592ea05ed9ed6fc70d2defab82ed7472bb
jay <bjchoi@lunasoft.co.kr>
2021-11-02 23:30:42 +0900

    상태에 따라 변하는 패딩 값추가

--- bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
+++ bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
@@ -5,19 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basicsodelab.ui.theme.BasicsCodelabTheme
+import androidx.compose.foundation.layout.Arrangement as Arrangement1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,10 +47,14 @@ fun Greeting(name: String) {
     ) {
         val expanded = remember { mutableStateOf(false) }
 
-        val extraPadding = if(expanded.value) 48.dp else 0.dp
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
 
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
