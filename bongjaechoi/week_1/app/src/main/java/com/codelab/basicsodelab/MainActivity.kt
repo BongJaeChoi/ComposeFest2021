8501fa68ffca75052df3cdace8feb280866ae790
jay <bjchoi@lunasoft.co.kr>
2021-11-14 12:37:18 +0900

    다크테마 구현

--- bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
+++ bongjaechoi/week_1/app/src/main/java/com/codelab/basicsodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.basicsodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -92,14 +93,19 @@ fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
-
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
